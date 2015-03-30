790691f803fde8b0bd1c075a1d758becdf30e058
Federico Di Nucci <dinu1389@gmail.com>
2015-03-30 15:36:49 -0300

    Solucion de test q fallaba de forma random

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,9 +4,9 @@ describe User do
   let!(:user) { create :user }
   let!(:organization) { create :organization }
   let!(:wish_item) { create :wish_item, organization: organization }
-  let!(:level1) { create :level, level: 1, from: 0, to: 10 }
-  let!(:level2) { create :level, level: 2, from: 10, to: 20 }
-  let!(:level3) { create :level, level: 3, from: 20, to: 30 }
+  let!(:level1) { create :level, level: 1, from: 0, to: 10, title: 'Buen Samaritano' }
+  let!(:level2) { create :level, level: 2, from: 10, to: 20, title: 'Robin Hood' }
+  let!(:level3) { create :level, level: 3, from: 20, to: 30, title: 'Teresa de Calcuta' }
 
   describe '#level' do
     let!(:donations) do
