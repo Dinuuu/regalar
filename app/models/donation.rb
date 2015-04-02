4399e1b9db14b444dae409e5837b2278fac73ff1
vickymesalcorta <m_vickymesalcorta@hotmail.com>
2015-04-02 15:42:21 -0300

    rebase

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -6,11 +6,7 @@ class Donation < ActiveRecord::Base
   validates :quantity, numericality: { greater_than: 0 }
   scope :for_organization, -> (organization) { where(organization: organization) }
   scope :for_user, -> (user) { where(user: user) }
-<<<<<<< HEAD
-  scope :done, -> { where(done: true) }
   scope :for_wish_item, -> (wish_item) { where(wish_item: wish_item) }
-=======
   scope :confirmed, -> { where(done: true) }
   scope :pending, -> { where(done: false) }
->>>>>>> Queda: Metodo en controller para acceder por URL a pending donations for one organization para un user loggeado y que pertenezca a la ong. URL: /organizations/:organization_id/donations/pending
 end
