ace0b442eee01b862aa46ab0525ba922851c6fb7
Federico Di Nucci <federico.dinucci@wolox.com.ar>
2016-04-17 12:09:09 -0300

    Solving Mail issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Regalar::Application.configure do
 
   # Mailer configuration
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'regalar.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'regalar-production.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default charset: "utf-8"
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -63,7 +63,7 @@ Regalar::Application.configure do
 
   # Mailer configuration
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'regalar.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'regalar-stage.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default charset: "utf-8"
